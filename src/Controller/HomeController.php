2f8a7d619468619e2dc9b51fbe0dd20fae5ca08b
Anais FRANCOIS <anais44.francois@gmail.com>
2020-10-09 11:41:06 +0200

    ajout api villes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
---
+++ src/Service/CitySearchService.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/*lass CitySearch
+{
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+}
